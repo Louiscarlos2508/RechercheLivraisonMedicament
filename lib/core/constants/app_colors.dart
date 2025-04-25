6b74dc21dfd304bdc1bbba659da3d74a93173c92
Louiscarlos2508 <carlosbusiness2508@gmail.com>
2025-04-25 15:55:16 +0000

    app_colors

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class AppColors{
+
+
+  static const primary_color = Color(0xFF4CAF93);
+  static const background_color = Color(0xFFF5F5F5);
+  static const surface_color = Color(0xFFFFFFFF);
+  static const success_color = Color(0xFF4CAF50);
+  static const warning_color = Color(0xFFFFC107);
+  static const alerte_color = Color(0xFFE53935);
+
+  static const textPrimary_color = Color(0xFF212121);
+  static const textSecondary_color = Color(0xFF757575);
+  static const textDisable_color = Color(0xFFBDBDBD);
+}
